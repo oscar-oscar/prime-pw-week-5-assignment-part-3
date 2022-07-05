7d56a3a3e6a085c72c92fac8808b51e962d53261
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-04 23:01:17 -0500

    created collection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+
+//show collection part 2 kyle's question
+//he's outputting via template literal vs longform string concatenation
+
+//find by artist is mjuch like carrot example in review session
+
+//show collection part
+// use for loop 
+// pass function using argument not the global
+//
+ for (let i = 0; i < music.length; i++)
+    let album = music[i];
+    console.log(album.title) // i will alway be index
+ // or use for of loop variable will always be 
+
+
+    
