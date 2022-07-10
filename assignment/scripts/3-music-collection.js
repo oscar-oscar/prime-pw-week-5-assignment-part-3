2abbaf4f82c8395518402863ca020af991ef8490
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-10 15:01:15 -0500

    updated checklist in read.me

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,73 +1,46 @@
 console.log('***** Music Collection *****')
 
 
-//show collection part 2 kyle's question
-//he's outputting via template literal vs longform string concatenation
+// //show collection part 2 kyle's question
+// //he's outputting via template literal vs longform string concatenation
 
-//find by artist is mjuch like carrot example in review session
+// //find by artist is mjuch like carrot example in review session
 
-//show collection part
-// use for loop 
-// pass function using argument not the global
-//
-//  for (let i = 0; i < music.length; i++)
-//     let album = music[i];
-//     console.log(album.title) // i will alway be index
-//  // or use for of loop variable will always be 
+// //show collection part
+// // use for loop 
+// // pass function using argument not the global
+// //
+// //  for (let i = 0; i < music.length; i++)
+// //     let album = music[i];
+// //     console.log(album.title) // i will alway be index
+// //  // or use for of loop variable will always be 
 
-//Create a variable `collection` that starts as an empty array.
-
-
-/**
- * addToCollection function to take in album title, artist and yearPublished as innput param
- * @param title
- * @param artist
- * @param yearPublished
- * return new object
- */
+// //Create a variable `collection` that starts as an empty array.
 
 
+let collection = [];
+console.log['', collection];
 
-//function name
-function addToCollection(title, artist, yearPublished){
-    let collection = [];
-    console.log('addToCollection');
-
-    // const newAlbum = {
-    //     title: 'No Code',
-    //     artist: 'Pearl Jam',
-    //     yearPublished: '1996',
-    // }
-    // newAlbum.push([collection]);
-}
-// addToCollection();
-// console.log('album added :')
-   
-
-
-
-
-    // bryns question 
-// function addPenguin( name, colors, size, isCute){
-//     const newPenguin = {
-//     name: name,
-//     colors: colors,
-//     size: size,
-//     isCute: isCute,
-//     }
-//     penguins.push(newPenguin);
-// }
-// addPenguin('Happy Feet', ['yellow'], 0.5, true);
-// console.log('newPenguin', newPenguin);
-
-// // const newAlbum = {
+// const pearlJam = {
 //     title: 'No Code',
 //     artist: 'Pearl Jam',
 //     yearPublished: '1996',
-
+     
 // };
-// collection.push(newAlbum);
 
-// }
-// addToCollection(newAlbum);
-// console.log('album added:' , newAlbum);
+// collection.push(pearlJam);
+
+function addToCollection(title, artist, yearPublished){
+    console.log('in addToCollection', title, artist, yearPublished)
+
+    const newRecord = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(newRecord)
+    return newRecord;
+};
+console.log(addToCollection('No Code', 'Pearl Jam', 1996));
+
+
--- readme.md
+++ readme.md
@@ -15,13 +15,13 @@ Update the `3-music-collection.js` file to do the following:
 
 ### Required Features
 
-- Create a variable `collection` that starts as an empty array.
+[x]- Create a variable `collection` that starts as an empty array.
 
-- Add a function named `addToCollection`. This function should:
-  - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-  - Create a new object having the above properties
-  - Add the new object to the end of the `collection` array
-  - Return the newly created object
+[x]-Add a function named `addToCollection`. This function should:
+[x]- Take in the album's `title`, `artist`, `yearPublished` as input parameters
+[x]- Create a new object having the above properties
+[x] - Add the new object to the end of the `collection` array
+[x] - Return the newly created object
 
 - Test the `addToCollection` function:
   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
