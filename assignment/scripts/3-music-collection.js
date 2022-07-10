980a6a9b959a91f24c224238f9ab4fa91fb7c7b3
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-10 14:59:59 -0500

    fixed code, I was creating individual object inside function. Coded so I just enter the info when function is called and added newRecord 'No Code' to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,10 +10,64 @@ console.log('***** Music Collection *****')
 // use for loop 
 // pass function using argument not the global
 //
- for (let i = 0; i < music.length; i++)
-    let album = music[i];
-    console.log(album.title) // i will alway be index
- // or use for of loop variable will always be 
+//  for (let i = 0; i < music.length; i++)
+//     let album = music[i];
+//     console.log(album.title) // i will alway be index
+//  // or use for of loop variable will always be 
+
+//Create a variable `collection` that starts as an empty array.
 
 
-    
+/**
+ * addToCollection function to take in album title, artist and yearPublished as innput param
+ * @param title
+ * @param artist
+ * @param yearPublished
+ * return new object
+ */
+
+
+
+//function name
+function addToCollection(title, artist, yearPublished){
+    let collection = [];
+    console.log('addToCollection');
+
+    // const newAlbum = {
+    //     title: 'No Code',
+    //     artist: 'Pearl Jam',
+    //     yearPublished: '1996',
+    // }
+    // newAlbum.push([collection]);
+}
+// addToCollection();
+// console.log('album added :')
+   
+
+
+
+
+    // bryns question 
+// function addPenguin( name, colors, size, isCute){
+//     const newPenguin = {
+//     name: name,
+//     colors: colors,
+//     size: size,
+//     isCute: isCute,
+//     }
+//     penguins.push(newPenguin);
+// }
+// addPenguin('Happy Feet', ['yellow'], 0.5, true);
+// console.log('newPenguin', newPenguin);
+
+// // const newAlbum = {
+//     title: 'No Code',
+//     artist: 'Pearl Jam',
+//     yearPublished: '1996',
+
+// };
+// collection.push(newAlbum);
+
+// }
+// addToCollection(newAlbum);
+// console.log('album added:' , newAlbum);
